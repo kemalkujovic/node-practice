f225c82071a1aeed5c3c58efe262d159ae1a03fb
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-06-23 22:05:05 +0200

    filtering paths

--- express-prac/app.js
+++ express-prac/app.js
@@ -1,24 +1,17 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+
+const adminRouter = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type'submit'>Send</button></input></form>"
-  );
-});
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("admin", adminRouter);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
