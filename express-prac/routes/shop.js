9a5125c321def688cc98e9b709745000b20f15bd
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-07-01 17:47:31 +0200

    prac express

--- express-prac/routes/shop.js
+++ express-prac/routes/shop.js
@@ -1,8 +1,9 @@
+const path = require("path");
 const express = require("express");
-
 const router = express.Router();
+const rootDir = require("../util/path");
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
