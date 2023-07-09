279abd6830e43691d357874e40f5b21a7be529fb
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-07-09 17:16:11 +0200

    replace with layout pug

--- express-prac/routes/admin.js
+++ express-prac/routes/admin.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const rootDir = require("../util/path");
 const products = [];
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
