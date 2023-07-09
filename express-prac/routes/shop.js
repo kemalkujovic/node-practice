aabec81db1535ac058b46fd9b92fa7c68bc831d3
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-07-09 16:14:07 +0200

    pug i && prac

--- express-prac/routes/shop.js
+++ express-prac/routes/shop.js
@@ -2,8 +2,13 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const rootDir = require("../util/path");
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
